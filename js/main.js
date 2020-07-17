fe41831ecf731ee325e282705cd8739eaf02b588
Serena <serenafayelombardo@gmail.com>
2020-07-17 11:24:36 +0200

    Added possibility to close menu when tabbing through list and exiting last or first menu item

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const menuButton = document.querySelector('.header__menu__button');
 const menuButtonImage = menuButton.querySelector('img');
 const menu = document.querySelector('.header__menu');
 const menuContainer = document.querySelector('.header__menu__container');
+const menuLinks = document.querySelectorAll('.header__menu__link');
 
 menuButton.addEventListener('click', () => {
   if (menu.classList.contains('hidden')) {
@@ -22,4 +23,17 @@ window.addEventListener('keyup', () => {
     menuButtonImage.src = 'images/icon-hamburger.svg';
     menuButton.setAttribute('aria-expanded', 'false');
   }
+});
+
+menuLinks.forEach((link) => {
+  link.addEventListener('focusout', (event) => {
+    if (event.relatedTarget.classList.contains('header__menu__link')) {
+      return;
+    } else {
+      menu.classList.add('hidden');
+      menuContainer.classList.remove('open');
+      menuButtonImage.src = 'images/icon-hamburger.svg';
+      menuButton.setAttribute('aria-expanded', 'false');
+    }
+  });
 });
