c0579084c66d805b178e3917b7334deb102e601d
Serena <serenafayelombardo@gmail.com>
2020-07-24 09:19:27 +0200

    Added functionality for the input for sending email to show message (error or not)

--- js/main.js
+++ js/main.js
@@ -4,6 +4,12 @@ const menu = document.querySelector('.header__menu');
 const menuContainer = document.querySelector('.header__menu__container');
 const menuLinks = document.querySelectorAll('.header__menu__link');
 
+const form = document.querySelector('.footer__newsletter__form');
+const emailInput = form.querySelector('.footer__newsletter__input');
+const tooltip = document.querySelector('.footer__tooltip');
+const tooltipTwo = document.querySelector('.footer__tooltip-two');
+
+// Functionality for Menu
 menuButton.addEventListener('click', () => {
   if (menu.classList.contains('hidden')) {
     menuButtonImage.src = 'images/icon-close.svg';
@@ -36,4 +42,25 @@ menuLinks.forEach((link) => {
       menuButton.setAttribute('aria-expanded', 'false');
     }
   });
+});
+
+//Functionality for sending email for newsletter
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const emailInputValue = event.target.email.value;
+  const regexEmail = /^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+  if (!regexEmail.test(emailInputValue)) {
+    tooltip.style.display = 'block';
+    emailInput.style.border = '1px solid hsl(0, 88%, 59%)';
+    emailInput.style.color = 'hsl(0, 88%, 59%)';
+  } else {
+    tooltip.style.display = 'none';
+    tooltipTwo.style.display = 'block';
+    emailInput.style.border = 'none';
+    emailInput.style.color = 'unset';
+    setTimeout(function () {
+      tooltipTwo.style.display = 'none';
+    }, 3000);
+    form.reset();
+  }
 });
